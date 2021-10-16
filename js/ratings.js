66bf425aa321c3080127b529ced3da17dcb37890
eshjay <olushola.abiodun@ovi123>
2021-10-16 01:37:14 +0100

    Ratings corrected

--- js/ratings.js
+++ js/ratings.js
@@ -10,8 +10,20 @@ const collect_ratings = () => {
 
     elements.forEach(element => {
         rating = parseInt(element.id.replace('star', '' ));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+       
 
     });
+    
+    if(rating.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
 
+    return ratings;
 }
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+});
 
