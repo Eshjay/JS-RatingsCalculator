371fa9c74eb440bfd1bbd4192aa122393b581cd4
eshjay <olushola.abiodun@ovi123>
2021-10-16 01:00:41 +0100

    first part

---
+++ js/ratings.js
@@ -0,0 +1,15 @@
+function collect_ratings() {
+    const ratings = {
+        "count": 0,
+        "sum": 0,
+        "average": 0
+    }
+    let rating = 0;
+}
+
+const elements = document.querySelectorAll("rating");
+
+elements.forEach(element => {
+    element.replace()
+
+});
